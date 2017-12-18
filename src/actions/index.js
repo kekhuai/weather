4d2ac6fe3fd4024f3fb671192955c3c7a7119d8d
Natthaphon Phanit <natthaphon.phanit@allianz.com>
2017-12-18 17:44:47 +0700

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '4e206b6b1a2a87be0f0f3912a004ae91';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
